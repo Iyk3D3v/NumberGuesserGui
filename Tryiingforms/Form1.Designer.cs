cb75d8e34e0275dea2ce631ce17e9b86e59ebc1b
Iyke <iykebolt@gmail.com>
2017-07-05 23:20:51 +0100

    Numberguesseer GUI

---
+++ Tryiingforms/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Tryiingforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a Number";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(152, 80);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 1;
+            this.txtNumber.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number Guesser Game";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tryiingforms/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tryiingforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //function to submit
+            Random myrandom = new Random();
+            int mainNum = myrandom.Next(1,10);//random variable
+            int number;//holds user input
+
+            
+            //to chk five inputs only
+
+            //try
+            //{
+            //    number = int.Parse(txtNumber.Text);
+            //    int newNmber = number;
+            //    return;
+
+            //}
+
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Invalid data, enter a number between 1 and 10");
+            //}
+
+
+            // txtNumber.Clear();
+            int num = 0;
+            do
+            {
+                int.TryParse(txtNumber.Text, out number);
+                //number = int.Parse(txtNumber.Text);
+
+                if (number == mainNum)
+                {
+                    txtNumber.Clear();
+                    MessageBox.Show("Correct!!");
+                    Application.Exit();
+
+                }
+
+                else if (number != mainNum)
+                {
+                    txtNumber.Clear();
+                    MessageBox.Show("InCorrect!! Try again");
+                    return;
+                }
+            }
+            while (num < 3);
+            num++;
+            
+
+            //int a = 3;
+
+            //for(int i = 5; i <5; i++)
+            //{
+
+            //}
+
+            
+               
+          
+        }
+
+       //problems faced
+       //to reduce num of imput times
+       //chk valodation
+    }
+}
